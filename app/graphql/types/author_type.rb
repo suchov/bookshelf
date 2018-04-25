b0c08cb2ac626bbe475e23fd60e1b7fd4a407f6e
suchov <artem.suchov@gmail.com>
2018-04-25 12:45:18 +0300

    create a full_name using model methods

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -5,5 +5,7 @@ Types::AuthorType = GraphQL::ObjectType.define do
   field :last_name, types.String
   field :birth_year, types.Int
   field :is_alive, types.Boolean
-  field :id, types.ID
+  field :id, !types.ID # !not allow the field to be nil - always must have value
+
+  field :full_name, types.String # custom added full name
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+
+  def full_name
+    [self.first_name, self.last_name].compact.join(' ')
+  end
 end
