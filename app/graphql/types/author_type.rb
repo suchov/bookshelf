4d577b885b72ca21a5a407127eee3085f72fbb06
suchov <artem.suchov@gmail.com>
2018-04-25 12:54:22 +0300

    create a custom field in graphql (not using model)

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,11 +1,14 @@
 Types::AuthorType = GraphQL::ObjectType.define do
   name "AuthorType"
 
-  field :first_name, types.String
+  field :first_name, types.String, description: "The first name of the author"
   field :last_name, types.String
   field :birth_year, types.Int
   field :is_alive, types.Boolean
   field :id, !types.ID # !not allow the field to be nil - always must have value
 
-  field :full_name, types.String # custom added full name
+  field :full_name, types.String do # custom graphql field
+    description "The full name of the author"
+    resolve ->(o, _, _) { [o.first_name, o.last_name].compact.join(' ') }
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,2 @@
 class Author < ApplicationRecord
-
-  def full_name
-    [self.first_name, self.last_name].compact.join(' ')
-  end
 end
