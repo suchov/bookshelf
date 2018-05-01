dc168f6f595676ac3e2b866ec4946def3d4a2de8
suchov <artem.suchov@gmail.com>
2018-05-01 22:46:05 +0300

    separate mutation with the help of Class to separate files

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,12 +1,5 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  field :createAuthor, Types::AuthorType do
-    argument :first_name, types.String
-    argument :last_name, types.String
-    argument :birth_year, types.Int
-    argument :is_alive, types.Boolean
-
-    resolve ->(obj, args, ctx) { Author.create args.to_h }
-  end
+  field :createAuthor, function: Mutations::CreateAuthor.new
 end
