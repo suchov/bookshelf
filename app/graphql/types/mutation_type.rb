0c2ff92c194c8c61071e68f7701a2c4c4142932f
suchov <artem.suchov@gmail.com>
2018-05-01 15:47:50 +0300

    create the first mutations - creations of the fields

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,12 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+  field :createAuthor, Types::AuthorType do
+    argument :first_name, types.String
+    argument :last_name, types.String
+    argument :birth_year, types.Int
+    argument :is_alive, types.Boolean
+
+    resolve ->(obj, args, ctx) { Author.create args.to_h }
   end
 end
